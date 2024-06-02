ed345abade852090090c6c075b91c8738952ba7d
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-06-02 16:50:45 +0000

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data fetched from API</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App
